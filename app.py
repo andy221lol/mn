a1f133e6ab5e37503002967c5f0753bf5ebcd987
andy221lol <74798291+andy221lol@users.noreply.github.com>
2025-02-14 21:26:47 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 from huggingface_hub import InferenceClient
 import os
+import { inject } from "@vercel/analytics"
 
 app = Flask(__name__)
 tkn = os.getenv("HFTKN")
